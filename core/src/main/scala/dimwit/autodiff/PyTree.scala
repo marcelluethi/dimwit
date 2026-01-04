01face89630495bd151eb55660a6073f55882978
Marcel Luethi <marcel.luethi@inf.ethz.ch>
2026-01-04 14:48:27 +0100

    support random key in PyTree
    This is important to allow keys as arguments in jitted functions

--- core/src/main/scala/dimwit/autodiff/PyTree.scala
+++ core/src/main/scala/dimwit/autodiff/PyTree.scala
@@ -2,6 +2,7 @@ package dimwit.autodiff
 
 import dimwit.tensor.{Tensor, Shape}
 import dimwit.jax.Jax
+import dimwit.random.Random
 
 import me.shadaj.scalapy.py
 import me.shadaj.scalapy.py.SeqConverters
@@ -21,6 +22,11 @@ object ToPyTree:
   given [T <: Tuple: Labels, V]: ToPyTree[Tensor[T, V]] with
     def toPyTree(t: Tensor[T, V]): Jax.PyAny = t.jaxValue
     def fromPyTree(p: Jax.PyAny): Tensor[T, V] = Tensor(p.as[Jax.PyDynamic])
+
+  // Random.Key instance - wraps and unwraps the JAX key
+  given ToPyTree[Random.Key] with
+    def toPyTree(k: Random.Key): Jax.PyAny = k.jaxKey
+    def fromPyTree(p: Jax.PyAny): Random.Key = Random.Key(p.as[Jax.PyDynamic])
 
   // Tuple instances - these should have lower priority than specific case classes
   given tupleInstance[A, B](using ta: ToPyTree[A], tb: ToPyTree[B]): ToPyTree[(A, B)] with
@@ -60,6 +66,8 @@ object ToPyTree:
       case _: Tensor[?, ?] =>
         // For tensors, delegate to the ToPyTree instance which has the proper type info
         compiletime.summonInline[ToPyTree[T]].fromPyTree(pyElem)
+      case _: Random.Key =>
+        Random.Key(pyElem.as[Jax.PyDynamic]).asInstanceOf[T]
       case _: String =>
         pyElem.as[String].asInstanceOf[T]
       case _: Int =>
@@ -88,6 +96,8 @@ object ToPyTree:
     inline erasedValue[T] match
       case _: Tensor[?, ?] =>
         elem.asInstanceOf[Tensor[?, ?]].jaxValue
+      case _: Random.Key =>
+        elem.asInstanceOf[Random.Key].jaxKey
       case _: String =>
         py.Dynamic.global.str(elem.asInstanceOf[String])
       case _: Int =>
